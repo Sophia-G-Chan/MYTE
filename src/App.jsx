894fa4bfa27643093bf3457c2627e409d78359ad
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-09-10 21:07:29 -0700

    dependencies added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
@@ -5,7 +6,9 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <div>
+        test
+        </div>
     </>
   )
 }
