c0e67e8999e077772b0a4ca68401b2d96e5f5219
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-09-11 17:09:26 -0700

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,69 @@ import Footer from "./components/Footer/Footer"
 import Home from "./pages/Home/Home"
 import NotFound from "./pages/NotFound"
 import Calendar from "./components/Calendar/Calendar"
-
+import { useSession, useSupabaseClient, useSessionContext } from "@supabase/auth-helpers-react"
+import DateTimePicker from "react-datetime-picker"
+import {useState} from 'react'
 
 function App() {
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd ] = useState(new Date());
 
+  const session = useSession();
+  console.log(session)
+  const supabase = useSupabaseClient();
+  const {isLoading} = useSessionContext();
 
+  if(isLoading){
+    return <></>
+  }
+
+  const googleSignIn = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        scopes: "https://www.googleapis.com/auth/calendar",
+
+      }
+    });
+    if (error) {
+      alert('Error logging in with Google provider with Supabase')
+      console.log(error)
+    }
+  }
+
+  const googleSignOut = async () => {
+    await supabase.auth.signOut();
+  }
+  console.log(start)
   return (
     <>
+      <div style={{ width: "400px", margin: "30px auto" }}>
+        {session ?
+          <>
+            <h3>Hey there {session.user.email}</h3>
+            <p>Start of your task</p>
+            <DateTimePicker onChange={setStart} value={start} />
+            <p>End date and time of your task</p>
+            <DateTimePicker onChange={setEnd} value={end} />
+            <button onClick={() => googleSignOut()}>Sign Out</button>
+          </>
+          :
+          <>
+            <button onClick={() => googleSignIn()}>Sign In with Google</button>
+          </>
+        }
+      </div>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/calendar" element={<Calendar/>}/>
-          <Route path="*" element={<NotFound/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>
+
     </>
   )
 }
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,12 +1,15 @@
 import ReactCalendar from 'react-calendar';
-import {Calendar, dateFnsLocalizer} from 'react-big-calendar';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import format from 'date-fns/format';
 import parse from 'date-fns/parse';
 import { startOfWeek } from 'date-fns/startOfWeek';
 import getDay from 'date-fns/getDay';
-import {useState} from 'react';
+import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import enUS from 'date-fns/locale/en-US'
+import toastCalendar from '@toast-ui/calendar'
+import '@toast-ui/calendar/dist/toastui-calendar.min.css'
+import './Calendar.scss'
 
 const locales = {
   'en-US': enUS,
@@ -25,18 +28,59 @@ const events = [
     title: "big project",
     allDay: true,
     start: new Date(2024,9,11),
-    end: new Date(2024, 9, 22)
+    end: new Date(2024,9,22)
+  },
+  {
+    title: "test",
+    start: new Date(2024,9,13),
+    end: new Date(2024,9,13)
   }
 ]
-function CalendarComponent () {
+console.log(events)
+
+function CalendarComponent() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+
+  const handleAddEvent = () => {
+    setAllEvents([...allEvents, newEvent])
+  }
+
+  const calendar = new toastCalendar('#calendar', {
+    defaultView: 'week',
+    template: {
+      time(event) {
+        const { start, end, title } = event;
+
+        return `<span style="color: white;">${formatTime(start)}~${formatTime(end)} ${title}</span>`;
+      },
+      allday(event) {
+        return `<span style="color: gray;">${event.title}</span>`;
+      },
+    },
+    calendars: [
+      {
+        id: 'cal1',
+        name: 'Personal',
+        backgroundColor: '#03bd9e',
+      },
+      {
+        id: 'cal2',
+        name: 'Work',
+        backgroundColor: '#00a9ff',
+      },
+    ],
+  });
   return (
     <div className='rounded border-2 border-blue'>
-      <h1>Calendar Component</h1>
+      {/* <h1>Calendar Component</h1>
       <ReactCalendar />
       <div className='custom-css'>
 
-      <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor="end" style={{all: "unset", height: 500, margin:"50px"}}/>
-      </div>
+
+      </div> */}
+
+      <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor="end" style={{ height: 500, margin: "50px" }} />
     </div>
   )
 }
