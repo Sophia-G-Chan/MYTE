a443efc91c8dc195c064af1e3737ec24125a5a73
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-09-15 16:50:14 -0700

    files created

---
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -0,0 +1,14 @@
+import './DeleteModal.scss';
+import { useState } from 'react';
+import CloseIcon from '../../assets/icons/close.svg';
+import DeleteIcon from '../../assets/icons/delete.svg'
+
+function DeleteModal() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default DeleteModal
